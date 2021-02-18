6337e091223178931fcf59515b5e7a1ecd457259
Jahanzaib Ali <jhanzeb.ali99@gmail.com>
2021-02-18 21:02:00 +0500

    React-Native WebRTC Peer Established

--- App/Src/Screens/Rooms.js
+++ App/Src/Screens/Rooms.js
@@ -1,286 +1,200 @@
-import {useFocusEffect} from '@react-navigation/native';
-import React, {useEffect, useState} from 'react';
-import {SafeAreaView, View, Dimensions, ScrollView} from 'react-native';
-import {
-  RTCPeerConnection,
-  mediaDevices,
-  RTCView,
-  RTCSessionDescription,
-  RTCIceCandidate,
-} from 'react-native-webrtc';
-import {useDispatch, useSelector} from 'react-redux';
+import React from 'react';
+import {View, SafeAreaView, Button, StyleSheet, Dimensions} from 'react-native';
+
+import {RTCPeerConnection, RTCView, mediaDevices} from 'react-native-webrtc';
 import {withSocket} from '../Utils/withSocket';
 const {height, width} = Dimensions.get('window');
 
 const Room = (props) => {
-  const [stream, setStream] = useState('');
-  const [partnerStream, setPartnerStream] = useState('');
-  const [userVideo, setUserVideo] = useState(null);
-  const [partnerVideo, setPartnerVideo] = useState({});
-  const [otherUser, setOtherUser] = useState('');
-  const [joinedUser, setJoinedUser] = useState('');
-  console.log(otherUser, 'doajda', joinedUser, ' OTHER JOINED');
-  React.useEffect(() => {
-    if (joinedUser && otherUser) {
-      callUser(otherUser, joinedUser);
-    }
-  }, [joinedUser, otherUser]);
-  // const [peerRefer, setPeerRefer] = useState('');
-  // const [userStream, setUserStream] = useState('');
-  let peerRefer = React.useRef(null);
-  // let otherUser = React.useRef();
-  // let partnerVideo = React.useRef();
-  // let userVideo = React.useRef();
-  let userStream = React.useRef();
-  console.log(joinedUser, 'JOINER');
-  console.log(otherUser, 'OTHER');
-  const {socket, route} = props;
+  const [localStream, setLocalStream] = React.useState();
+  const [remoteStream, setRemoteStream] = React.useState();
+  const [cachedLocalPC, setCachedLocalPC] = React.useState();
+  const [cachedRemotePC, setCachedRemotePC] = React.useState();
 
-  const dispatch = useDispatch();
-  const {Stream, streams} = useSelector((state) => state.Video);
-  const [referer, setreferer] = useState('');
-  console.log(referer);
-  useEffect(() => {
-    if (referer) {
-      console.log(referer, 'useEffect Referer');
-      peerRefer.current = referer;
-    }
-  }, [referer]);
-  useEffect(() => {
-    if (userVideo) {
-      console.log(userVideo, 'useEffect userVideo');
-      userStream.current = userVideo;
-    }
-  }, [userVideo]);
-  function handleRecieveCall(incoming) {
-    console.log(incoming, 'INCOME HANDLE RECIEVER');
-    const peer = createPeer();
-    setreferer(peer);
-    // peerRefer.current = peer;
-    const desc = new RTCSessionDescription(incoming.sdp);
-    peer
-      .setRemoteDescription(desc)
-      .then(() => {
-        userStream.current
-          .getTracks()
-          .forEach((track) => peer.addStream(track, userStream));
-      })
-      .then(() => {
-        console.log('THEN IN CREATE ANSWER');
-        return peer.createAnswer();
-      })
-      .then((answer) => {
-        console.log(answer, 'ans');
-        return peer.setLocalDescription(answer);
-      })
-      .then(() => {
-        const payload = {
-          target: incoming.caller,
-          caller: socket.id,
-          sdp: peer.localDescription,
-        };
-        socket.emit('answer', payload);
-      })
-      .catch((e) => console.log(e, 'EEE'));
-  }
-  function handleAnswer(message) {
-    console.log(message, 'MESSAGE ANSWER');
-    const desc = new RTCSessionDescription(message.sdp);
-    console.log(desc, 'DEscription');
-    peerRefer.current.setRemoteDescription(desc).catch((e) => console.log(e));
-  }
-  function handleICECanditateEvent(candidate) {
-    if (candidate) {
-      const payload = {
-        target: otherUser,
-        candidate: candidate,
-      };
-      socket.emit('ice-candidate', payload);
-    }
-  }
-  const handleNewICECanditateMsg = (incoming) => {
-    console.log(incoming, 'ICO');
-    const candidate = new RTCIceCandidate(incoming);
-    peerRefer.current
-      .addIceCandidate(candidate)
-      .catch((e) => console.log(e, 'S'));
+  const [isMuted, setIsMuted] = React.useState(false);
+
+  const startLocalStream = async () => {
+    // isFront will determine if the initial camera should face user or environment
+    const isFront = true;
+    const devices = await mediaDevices.enumerateDevices();
+
+    const facing = isFront ? 'front' : 'environment';
+    const videoSourceId = devices.find(
+      (device) => device.kind === 'videoinput' && device.facing === facing,
+    );
+    const facingMode = isFront ? 'user' : 'environment';
+    const constraints = {
+      audio: true,
+      video: {
+        mandatory: {
+          minWidth: 500, // Provide your own width, height and frame rate here
+          minHeight: 300,
+          minFrameRate: 30,
+        },
+        facingMode,
+        optional: videoSourceId ? [{sourceId: videoSourceId}] : [],
+      },
+    };
+    const newStream = await mediaDevices.getUserMedia(constraints);
+    setLocalStream(newStream);
   };
-  const handleTrackEvent = (e) => {
-    console.log(e, 'E');
-    setPartnerVideo(e.streams[0]);
-  };
-  React.useEffect(() => {
-    let isFront = true;
-    mediaDevices.enumerateDevices().then((sourceInfos) => {
-      let videoSourceId;
-      for (let i = 0; i < sourceInfos.length; i++) {
-        const sourceInfo = sourceInfos[i];
-        if (
-          sourceInfo.kind == 'videoinput' &&
-          sourceInfo.facing == (isFront ? 'front' : 'environment')
-        ) {
-          videoSourceId = sourceInfo.deviceId;
+
+  const startCall = async () => {
+    // You'll most likely need to use a STUN server at least. Look into TURN and decide if that's necessary for your project
+    const configuration = {iceServers: [{url: 'stun:stun.l.google.com:19302'}]};
+    const localPC = new RTCPeerConnection(configuration);
+    const remotePC = new RTCPeerConnection(configuration);
+
+    // could also use "addEventListener" for these callbacks, but you'd need to handle removing them as well
+    localPC.onicecandidate = (e) => {
+      try {
+        console.log('localPC icecandidate:', e.candidate);
+        if (e.candidate) {
+          remotePC.addIceCandidate(e.candidate);
         }
+      } catch (err) {
+        console.error(`Error adding remotePC iceCandidate: ${err}`);
       }
-      mediaDevices
-        .getUserMedia({
-          audio: true,
-          video: {
-            width: 640,
-            height: 480,
-            frameRate: 30,
-            facingMode: isFront ? 'user' : 'environment',
-            deviceId: videoSourceId,
-          },
-        })
-        .then((stream) => {
-          userStream.current = stream;
-          // userVideo.current = stream;
-          setUserVideo(stream);
-          socket.emit('join room', route?.params?.id);
-          socket.on('other user', (userID) => callUser(userID));
-          socket.on('user joined', (userID) => setJoinedUser(userID));
-          // socket.on('user joined', (userID) => setJoinedUser(userID);
-          // socket.emit('offer', (data) => handleRecieveCall(data));
-          socket.on('offer', handleRecieveCall);
-          socket.on('answer', handleAnswer);
-          socket.on('ice-candidate', handleNewICECanditateMsg);
-          // console.log(stream, "STREAM")
-          // setStream(stream);
-          // setUserStream(stream);
-          //   dispatch(joinRoom(stream));
-          // Got stream!
-        })
-        .catch((error) => {
-          console.log(error, 'E');
-          // Log error
-        });
-    });
-    // return () => {
-    //   socket.off('join room');
-    //   socket.off('other user');
-    //   socket.off('user joined');
-    //   socket.off('offer');
-    //   socket.off('answer');
-    //   socket.off('ice-candidate');
-    // };
-  }, []);
-  const callUser = (target) => {
-    console.log(target, 'Other USERS');
+    };
+    remotePC.onicecandidate = (e) => {
+      try {
+        console.log('remotePC icecandidate:', e.candidate);
+        if (e.candidate) {
+          localPC.addIceCandidate(e.candidate);
+        }
+      } catch (err) {
+        console.error(`Error adding localPC iceCandidate: ${err}`);
+      }
+    };
+    remotePC.onaddstream = (e) => {
+      console.log('remotePC tracking with ', e);
+      if (e.stream && remoteStream !== e.stream) {
+        console.log('RemotePC received the stream', e.stream);
+        setRemoteStream(e.stream);
+      }
+    };
 
-    const peer = createPeer(target);
-    setreferer(peer);
-    handleNegotiationNeededEvent(target, peer);
-    peerRefer.current = peer;
-    userStream.current
-      .getTracks()
-      .forEach((track) => peer.addStream(track, userStream));
+    // AddTrack not supported yet, so have to use old school addStream instead
+    // newStream.getTracks().forEach(track => localPC.addTrack(track, newStream));
+    localPC.addStream(localStream);
+    try {
+      const offer = await localPC.createOffer();
+      console.log('Offer from localPC, setLocalDescription');
+      await localPC.setLocalDescription(offer);
+      console.log('remotePC, setRemoteDescription');
+      await remotePC.setRemoteDescription(localPC.localDescription);
+      console.log('RemotePC, createAnswer');
+      const answer = await remotePC.createAnswer();
+      console.log(`Answer from remotePC: ${answer.sdp}`);
+      console.log('remotePC, setLocalDescription');
+      await remotePC.setLocalDescription(answer);
+      console.log('localPC, setRemoteDescription');
+      await localPC.setRemoteDescription(remotePC.localDescription);
+    } catch (err) {
+      console.error(err);
+    }
+    setCachedLocalPC(localPC);
+    setCachedRemotePC(remotePC);
   };
-  const createPeer = (userID) => {
-    const peer = new RTCPeerConnection({
-      iceServers: [{url: 'stun:stun.l.google.com:19302'}],
+
+  const switchCamera = () => {
+    localStream.getVideoTracks().forEach((track) => track._switchCamera());
+  };
+
+  // Mutes the local's outgoing audio
+  const toggleMute = () => {
+    if (!remoteStream) return;
+    localStream.getAudioTracks().forEach((track) => {
+      console.log(track.enabled ? 'muting' : 'unmuting', ' local track', track);
+      track.enabled = !track.enabled;
+      setIsMuted(!track.enabled);
     });
-    peer.onicecandidate = handleICECanditateEvent;
-    peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID, peer);
-    peer.onaddstream = (e) => handleTrackEvent(e);
-    return peer;
   };
-  const handleNegotiationNeededEvent = (userID, peer) => {
-    peer
-      .createOffer()
-      .then((offer) => {
-        console.log(offer, 'off');
-        return peer.setLocalDescription(offer);
-      })
-      .then(() => {
-        const payload = {
-          target: userID,
-          caller: socket.id,
-          sdp: peer.localDescription,
-        };
-        socket.emit('offer', payload);
-      })
-      .catch((e) => {
-        console.log(e, 'pmlsda');
-      });
+
+  const closeStreams = () => {
+    if (cachedLocalPC) {
+      cachedLocalPC.removeStream(localStream);
+      cachedLocalPC.close();
+    }
+    if (cachedRemotePC) {
+      cachedRemotePC.removeStream(remoteStream);
+      cachedRemotePC.close();
+    }
+    setLocalStream();
+    setRemoteStream();
+    setCachedRemotePC();
+    setCachedLocalPC();
   };
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={{flex: 1, justifyContent: 'flex-start', padding: 10}}>
-        <View
-          style={{
-            flex: 1,
-            justifyContent: 'center',
-            height: height * 0.5,
-            borderColor: 'yellow',
-            borderWidth: 4,
-          }}>
-          {userVideo && userVideo ? (
-            <RTCView
-              style={{width, height: height * 0.4}}
-              streamURL={userVideo?.toURL()}
-            />
-          ) : (
-            <></>
-          )}
+    <SafeAreaView style={styles.container}>
+      {!localStream && (
+        <Button title="Click to start stream" onPress={startLocalStream} />
+      )}
+      {localStream && (
+        <Button
+          title="Click to start call"
+          onPress={startCall}
+          disabled={!!remoteStream}
+        />
+      )}
+
+      {localStream && (
+        <View style={styles.toggleButtons}>
+          <Button title="Switch camera" onPress={switchCamera} />
+          <Button
+            title={`${isMuted ? 'Unmute' : 'Mute'} stream`}
+            onPress={toggleMute}
+            disabled={!remoteStream}
+          />
         </View>
-        <View style={{flex: 1, backgroundColor: 'black'}}>
-          <ScrollView horizontal style={{padding: 10}}>
-            <>
-              <>
-                {/* {partnerStream && (
-                  <View
-                    style={{
-                      width: 280,
-                      backgroundColor: 'red',
-                      borderWidth: 1,
-                      borderColor: '#fff',
-                      marginRight: 10,
-                      padding: 5,
-                    }}>
-                    <RTCView
-                      streamURL={partnerStream.toURL()}
-                      style={{width: 180, height: height * 0.4}}
-                    />
-                  </View>
-                )} */}
-                {/* {partnerStream && (
-                  <View
-                    key={index}
-                    style={{
-                      width: 280,
-                      backgroundColor: 'red',
-                      borderWidth: 1,
-                      borderColor: '#fff',
-                      marginRight: 10,
-                      padding: 5,
-                    }}>
-                    <RTCView
-                      streamURL={partnerStream.toURL()}
-                      style={{width: 180, height: height * 0.4}}
-                    />
-                  </View>
-                )} */}
-              </>
-            </>
-            <>
-              <View
-                style={{
-                  width: 280,
-                  backgroundColor: 'blue',
-                  borderWidth: 1,
-                  borderColor: '#fff',
-                  marginRight: 10,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}></View>
-            </>
-          </ScrollView>
-        </View>
+      )}
+
+      <View style={styles.rtcview}>
+        {localStream && (
+          <RTCView style={styles.rtc} streamURL={localStream.toURL()} />
+        )}
       </View>
+      <View style={styles.rtcview}>
+        {remoteStream && (
+          <RTCView style={styles.rtc} streamURL={remoteStream.toURL()} />
+        )}
+      </View>
+      <Button
+        title="Click to stop call"
+        onPress={closeStreams}
+        disabled={!remoteStream}
+      />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#313131',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100%',
+  },
+  text: {
+    fontSize: 30,
+  },
+  rtcview: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '40%',
+    width: '80%',
+    backgroundColor: 'black',
+  },
+  rtc: {
+    width: '80%',
+    height: '100%',
+  },
+  toggleButtons: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+});
 
 export default withSocket(Room);
